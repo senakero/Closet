04352b248e0f22b705c90894cf3f9b7be33f955a
古賀聖奈 <senakoga@gmail.com>
2016-01-20 19:56:42 +0900

    first commit

---
+++ Closet/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Closet
+//
+//  Created by 古賀聖奈 on 2016/01/20.
+//  Copyright © 2016年 Sena.org. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
