878ed57093de9d0924220888a2ac18f4e5d16d99
古賀聖奈 <senakoga@gmail.com>
2016-02-17 21:06:04 +0900

    AddViewControllerのボタンの色表示を変更

--- Closet/AddViewController.swift
+++ Closet/AddViewController.swift
@@ -12,65 +12,266 @@ class AddViewController: UIViewController {
     
     var a = String()
     @IBOutlet var addlabel: UILabel!
-
+    
+    
+    
+    @IBOutlet var addtopsbutton: UIButton!
+    
+    @IBOutlet var addbottomsbutton: UIButton!
+    
+    @IBOutlet var addshoesbutton: UIButton!
+    
+    @IBOutlet var addsocksbutton: UIButton!
+    
+    @IBOutlet var addaccessoriesbutton: UIButton!
+    
+    @IBOutlet var addothersbutton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         // Do any additional setup after loading the view.
+        
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
-    @IBAction func snap() {
-       
-        a = "snap"
-        addlabel.text = String(a)
+    func topscolor() {
+        if a == "tops" {
+            
+            self.addbottomsbutton.backgroundColor = UIColor.whiteColor()
+            self.addbottomsbutton.setTitleColor(UIColor.blackColor(), forState: .Normal)
+            
+            self.addshoesbutton.backgroundColor = UIColor.whiteColor()
+            self.addshoesbutton.setTitleColor(UIColor.blackColor(), forState: .Normal)
+            
+            self.addsocksbutton.backgroundColor = UIColor.whiteColor()
+            self.addsocksbutton.setTitleColor(UIColor.blackColor(), forState: .Normal)
+            
+            self.addaccessoriesbutton.backgroundColor = UIColor.whiteColor()
+            self.addaccessoriesbutton.setTitleColor(UIColor.blackColor(), forState: .Normal)
+            
+            self.addothersbutton.backgroundColor = UIColor.whiteColor()
+            self.addothersbutton.setTitleColor(UIColor.blackColor(), forState: .Normal)
+            
+            self.addtopsbutton.backgroundColor = UIColor.blackColor()
+            self.addtopsbutton.setTitleColor(UIColor.whiteColor(), forState: .Normal)
+            
+        }else{
+            
+            self.addtopsbutton.backgroundColor = UIColor.blackColor()
+            self.addtopsbutton.setTitleColor(UIColor.whiteColor(), forState: .Normal)
+            
+        }
+        
+    }
     
+    func bottomscolor() {
+        if a == "bottoms"{
+            
+            self.addtopsbutton.backgroundColor = UIColor.whiteColor()
+            self.addtopsbutton.setTitleColor(UIColor.blackColor(), forState: .Normal)
+            
+            self.addshoesbutton.backgroundColor = UIColor.whiteColor()
+            self.addshoesbutton.setTitleColor(UIColor.blackColor(), forState: .Normal)
+            
+            self.addsocksbutton.backgroundColor = UIColor.whiteColor()
+            self.addsocksbutton.setTitleColor(UIColor.blackColor(), forState: .Normal)
+            
+            self.addaccessoriesbutton.backgroundColor = UIColor.whiteColor()
+            self.addaccessoriesbutton.setTitleColor(UIColor.blackColor(), forState: .Normal)
+            
+            self.addothersbutton.backgroundColor = UIColor.whiteColor()
+            self.addothersbutton.setTitleColor(UIColor.blackColor(), forState: .Normal)
+            
+            self.addbottomsbutton.backgroundColor = UIColor.blackColor()
+            self.addbottomsbutton.setTitleColor(UIColor.whiteColor(), forState: .Normal)
+            
+        }else{
+            self.addbottomsbutton.backgroundColor = UIColor.blackColor()
+            self.addbottomsbutton.setTitleColor(UIColor.whiteColor(), forState: .Normal)
+            
+        }
+        
+    }
+    
+    func shoescolor() {
+        
+        if a == "shoes"{
+            
+            self.addtopsbutton.backgroundColor = UIColor.whiteColor()
+            self.addtopsbutton.setTitleColor(UIColor.blackColor(), forState: .Normal)
+            
+            self.addbottomsbutton.backgroundColor = UIColor.whiteColor()
+            self.addbottomsbutton.setTitleColor(UIColor.blackColor(), forState: .Normal)
+            
+            self.addsocksbutton.backgroundColor = UIColor.whiteColor()
+            self.addsocksbutton.setTitleColor(UIColor.blackColor(), forState: .Normal)
+            
+            self.addaccessoriesbutton.backgroundColor = UIColor.whiteColor()
+            self.addaccessoriesbutton.setTitleColor(UIColor.blackColor(), forState: .Normal)
+            
+            self.addothersbutton.backgroundColor = UIColor.whiteColor()
+            self.addothersbutton.setTitleColor(UIColor.blackColor(), forState: .Normal)
+            
+            self.addshoesbutton.backgroundColor = UIColor.blackColor()
+            self.addshoesbutton.setTitleColor(UIColor.whiteColor(), forState: .Normal)
+            
+        }else{
+            self.addshoesbutton.backgroundColor = UIColor.blackColor()
+            self.addshoesbutton.setTitleColor(UIColor.whiteColor(), forState: .Normal)
+            
+        }
+    }
+    
+    func sockscolor() {
+        
+        if a == "socks"{
+            self.addtopsbutton.backgroundColor = UIColor.whiteColor()
+            self.addtopsbutton.setTitleColor(UIColor.blackColor(), forState: .Normal)
+            
+            self.addbottomsbutton.backgroundColor = UIColor.whiteColor()
+            self.addbottomsbutton.setTitleColor(UIColor.blackColor(), forState: .Normal)
+            
+            self.addshoesbutton.backgroundColor = UIColor.whiteColor()
+            self.addshoesbutton.setTitleColor(UIColor.blackColor(), forState: .Normal)
+            
+            self.addaccessoriesbutton.backgroundColor = UIColor.whiteColor()
+            self.addaccessoriesbutton.setTitleColor(UIColor.blackColor(), forState: .Normal)
+            
+            self.addothersbutton.backgroundColor = UIColor.whiteColor()
+            self.addothersbutton.setTitleColor(UIColor.blackColor(), forState: .Normal)
+            
+            self.addsocksbutton.backgroundColor = UIColor.blackColor()
+            self.addsocksbutton.setTitleColor(UIColor.whiteColor(), forState: .Normal)
+            
+        }else{
+            self.addsocksbutton.backgroundColor = UIColor.blackColor()
+            self.addsocksbutton.setTitleColor(UIColor.whiteColor(), forState: .Normal)
+            
+        }
+    }
+    
+    func accessoriescolor() {
+        
+        if a == "accessories"{
+            self.addtopsbutton.backgroundColor = UIColor.whiteColor()
+            self.addtopsbutton.setTitleColor(UIColor.blackColor(), forState: .Normal)
+            
+            self.addbottomsbutton.backgroundColor = UIColor.whiteColor()
+            self.addbottomsbutton.setTitleColor(UIColor.blackColor(), forState: .Normal)
+            
+            self.addshoesbutton.backgroundColor = UIColor.whiteColor()
+            self.addshoesbutton.setTitleColor(UIColor.blackColor(), forState: .Normal)
+            
+            self.addsocksbutton.backgroundColor = UIColor.whiteColor()
+            self.addsocksbutton.setTitleColor(UIColor.blackColor(), forState: .Normal)
+            
+            self.addothersbutton.backgroundColor = UIColor.whiteColor()
+            self.addothersbutton.setTitleColor(UIColor.blackColor(), forState: .Normal)
+            
+            self.addaccessoriesbutton.backgroundColor = UIColor.blackColor()
+            self.addaccessoriesbutton.setTitleColor(UIColor.whiteColor(), forState: .Normal)
+            
+        }else{
+            self.addaccessoriesbutton.backgroundColor = UIColor.blackColor()
+            self.addaccessoriesbutton.setTitleColor(UIColor.whiteColor(), forState: .Normal)
+            
+        }
+        
+    }
+    
+    func otherscolor() {
+        
+        if a == "Others"{
+            self.addtopsbutton.backgroundColor = UIColor.whiteColor()
+            self.addtopsbutton.setTitleColor(UIColor.blackColor(), forState: .Normal)
+            
+            self.addbottomsbutton.backgroundColor = UIColor.whiteColor()
+            self.addbottomsbutton.setTitleColor(UIColor.blackColor(), forState: .Normal)
+            
+            self.addshoesbutton.backgroundColor = UIColor.whiteColor()
+            self.addshoesbutton.setTitleColor(UIColor.blackColor(), forState: .Normal)
+            
+            self.addsocksbutton.backgroundColor = UIColor.whiteColor()
+            self.addsocksbutton.setTitleColor(UIColor.blackColor(), forState: .Normal)
+            
+            self.addaccessoriesbutton.backgroundColor = UIColor.whiteColor()
+            self.addaccessoriesbutton.setTitleColor(UIColor.blackColor(), forState: .Normal)
+            
+            self.addothersbutton.backgroundColor = UIColor.blackColor()
+            self.addothersbutton.setTitleColor(UIColor.whiteColor(), forState: .Normal)
+            
+        }else{
+            self.addothersbutton.backgroundColor = UIColor.blackColor()
+            self.addothersbutton.setTitleColor(UIColor.whiteColor(), forState: .Normal)
+            
+        }
+        
+    }
+    
+    
+    @IBAction func snap() {
+        
+        a = "snap"
+        addlabel.text = a
+        
     }
     
     @IBAction func addtops() {
         
         a = "tops"
         addlabel.text = String(a)
-    
+        
+        topscolor()
+        
     }
     
     @IBAction func addbottoms() {
         
         a = "bottoms"
-        addlabel.text = String(a)
-    
+        addlabel.text = a
+        
+        bottomscolor()
+        
     }
     
     @IBAction func addshoes() {
         
+        
         a = "shoes"
-        addlabel.text = String(a)
+        addlabel.text = a
+        
+        shoescolor()
         
     }
     
     @IBAction func addsocks() {
         
         a = "socks"
-        addlabel.text = String(a)
-    
+        addlabel.text = a
+        
+        sockscolor()
+        
     }
     
     @IBAction func addaccessories() {
-    
-        a = "accessories"
-        addlabel.text = String(a)
         
+        
+        a = "accessories"
+        addlabel.text = a
+        
+        accessoriescolor()
     }
     
     @IBAction func addothers() {
         
         a = "Others"
-        addlabel.text = String(a)
-    
+        addlabel.text = a
+        otherscolor()
     }
     
     @IBAction func save() {
--- Closet/HomeViewController.swift
+++ Closet/HomeViewController.swift
@@ -11,80 +11,104 @@ import UIKit
 class HomeViewController: UIViewController {
     
     var identifier: Int!
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         // Do any additional setup after loading the view.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
         
     }
     
+    // サブページに画面遷移
     func move(){
         
-        let targetViewController = self.storyboard!.instantiateViewControllerWithIdentifier( "target" ) as! UIViewController
+        let targetViewController = self.storyboard!.instantiateViewControllerWithIdentifier("target")
         
         self.presentViewController(targetViewController, animated: true, completion: nil)
-      
+        
+    }
+    
+    @IBAction func plus(){
+        identifier = 9
+        
     }
     
     
     @IBAction func tops(){
-    
+        
         identifier = 0
-        move()
+//        move()
+        
+                performSegueWithIdentifier("toPage", sender: self)
     }
     
     @IBAction func bottoms(){
-     
-        identifier = 1
-        move()
         
+        identifier = 1
+//        move()
+        
+        performSegueWithIdentifier("toPage", sender: self)
+
     }
     
     @IBAction func shoes(){
-     
+        
         identifier = 2
-        move()
+//        move()
+        
+        performSegueWithIdentifier("toPage", sender: self)
+
     }
     
     @IBAction func socks(){
         
         identifier = 3
-        move()
+//        move()
+        
+        performSegueWithIdentifier("toPage", sender: self)
+
     }
     
     @IBAction func accessories(){
         
         identifier = 4
-        move()
+//        move()
+        
+        performSegueWithIdentifier("toPage", sender: self)
+
     }
     
     @IBAction func others(){
         
         identifier = 5
-        move()
+//        move()
+        
+        performSegueWithIdentifier("toPage", sender: self)
+
     }
     
     @IBAction func calender(){
         
     }
-
+    
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
         
-        var topsViewController = segue.destinationViewController as!
-            TopsViewController
-    
-        
-        topsViewController.identifier = self.identifier
+        if segue.identifier == "toPage" {
+            
+            print("aaa1")
+            
+            let topsViewController = segue.destinationViewController as! TopsViewController
+            
+            topsViewController.identifier = self.identifier
+            
+            
+        }
         
     }
     
-    }
-    
-
-
+}
